68bef6dafa500f1e0f0fa67e496bf594127b79b7
manosmin <manosmin@hotmail.com>
2025-06-28 22:57:48 +0300

    v1.0

--- coingecko-backend/utils/config.js
+++ coingecko-backend/utils/config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 const API_KEY = process.env.COINGECKO_API_KEY
 
 module.exports = { PORT, API_KEY }
