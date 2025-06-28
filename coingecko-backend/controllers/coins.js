d7ce24eb3e61b01e3da63f4ec1f1eb29c37a8ca4
manosmin <manosmin@hotmail.com>
2025-06-28 23:37:00 +0300

    v1.0

--- coingecko-backend/controllers/coins.js
+++ coingecko-backend/controllers/coins.js
@@ -15,39 +15,34 @@ coinsRouter.get('/markets', async (req, res) => {
     return res.status(400).json({ error: 'Invalid query parameters. `page` must be a positive integer and `per_page` must be between 1 and 250.' })
   }
 
-  try {
-    const data = await proxy.fetchCoinMarketData(page, per_page)
+  const data = await proxy.fetchCoinMarketData(page, per_page)
 
-    if (!data) {
-      logger.info('No data received from CoinGecko API')
-      return res.status(404).json({ error: 'No market data available.' })
-    }
-
-    logger.info('Market data fetched successfully')
-    res.status(200).json(data)
-  } catch (error) {
-    logger.axiosError('Error fetching data: ', error)
-    res.status(500).json({ error: 'Failed to fetch market data.' })
+  if (data.error) {
+    return res.status(500).json({ error: data.error })
   }
+
+  logger.info('Market data fetched successfully')
+  res.status(200).json(data)
+
 })
 
 coinsRouter.get('/:id', async (req, res) => {
   const coinId = req.params.id
 
-  try {
-    const data = await proxy.fetchCoinById(coinId)
+  const data = await proxy.fetchCoinById(coinId)
 
-    if (!data) {
-      logger.info(`No data found for ${coinId}`)
-      return res.status(404).json({ error: 'Coin not found.' })
-    }
-
-    logger.info(`Data for ${coinId} fetched successfully`)
-    res.status(200).json(data)
-  } catch (error) {
-    logger.axiosError(`Error fetching data for ${coinId}: `, error)
-    res.status(500).json({ error: 'Failed to fetch coin data.' })
+  if(data.status === 500) {
+    return res.status(500).json({ error: 'Failed to fetch coin data.' })
   }
+
+  if (data.status === 404) {
+    logger.info(`No data found for ${coinId}`)
+    return res.status(404).json({ error: data.error })
+  }
+
+  logger.info(`Data for ${coinId} fetched successfully`)
+  res.status(200).json(data)
+
 })
 
 module.exports = coinsRouter
